270aad3a5b0892617d2e692676d01847aa64a025
Services <95220392+Naylor-Sciencie@users.noreply.github.com>
2025-08-06 23:55:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { TelegramClient } = require ("telegram");
+const { StringSession } = require ("telegram/sessions");
+const readline = require ("readline");
+
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
+const { apiId, apiHash } = require ("./config.js");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+  await client.start({
+    phoneNumber: async () =>
+      new Promise((resolve) =>
+        rl.question("Please enter your number: ", resolve)
+      ),
+    password: async () =>
+      new Promise((resolve) =>
+        rl.question("Please enter your password: ", resolve)
+      ),
+    phoneCode: async () =>
+      new Promise((resolve) =>
+        rl.question("Please enter the code you received: ", resolve)
+      ),
+    onError: (err) => console.log(err),
+  });
+  console.log("You should now be connected.");
+  console.log(client.session.save()); // Save this string to avoid logging in again
+  await client.sendMessage("me", { message: "Hello!" });
+})();
